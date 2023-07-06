1bad9d333428af3c6125dfe60eb28925c9f638d5
egoing <egoing@gmail.com>
2023-07-06 18:52:03 +0900

    코드가 수정된 횟수를 카운팅해서 이를 일정한 시간(0.5초)만큼 계산해서 카운트 다운함

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,45 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
-
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "tick-tock-code" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('tick-tock-code.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from tick-tock-code!');
-	});
-
-	context.subscriptions.push(disposable);
+import * as vscode from "vscode";
+let countdownTimer:any;
+function startCountdown(diff:number) {
+	let countdown = diff;
+	if(countdownTimer){
+		clearInterval(countdownTimer);
+	}
+	countdownTimer = setInterval(() => {
+			if (countdown <= 0) {
+					clearInterval(countdownTimer);
+					clockStatusUI.text = `$(watch) 0`;
+			} else {
+					clockStatusUI.text = `$(watch) ${countdown}s`;
+					countdown--;
+			}
+	}, 1000);
 }
+let clockStatusUI: vscode.StatusBarItem;
+let defaultDelta = 0.5;
+export function activate(context: vscode.ExtensionContext) {
+	let previousContent: string | undefined = undefined;
+	let typingCount = 0;
+  vscode.workspace.onDidOpenTextDocument((document: vscode.TextDocument) => {
+		previousContent = document.getText();
+	});	
+	vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
+    // 텍스트 문서가 변경될 때마다 실행 횟수 증가
+		if (event.contentChanges.length > 0) {
+			typingCount = typingCount + defaultDelta;
+		}
+	});
+  vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
+    if (typingCount > 0) {
+				clockStatusUI.show();
+        startCountdown(Math.ceil(typingCount));
+    }
+    typingCount = 0; // Save 후 실행 횟수 초기화
+	});
+	clockStatusUI = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	context.subscriptions.push(vscode.commands.registerCommand('extension.Tick-tock-clock: stop', () => {
+		vscode.window.showInformationMessage('Hello!');
+	}));
 
-// This method is called when your extension is deactivated
+}
 export function deactivate() {}
