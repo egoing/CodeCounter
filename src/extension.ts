d99dc18702170f694d1217161bcc76537257f9d0
egoing <egoing@gmail.com>
2023-07-06 22:04:19 +0900

    리팩토링

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,67 @@
 import * as vscode from "vscode";
-let countdownTimer:any;
-function startCountdown(diff:number) {
-	let countdown = diff;
-	if(countdownTimer){
-		clearInterval(countdownTimer);
-	}
-	countdownTimer = setInterval(() => {
-			if (countdown <= 0) {
-					clearInterval(countdownTimer);
-					clockStatusUI.text = `$(watch) 0`;
-			} else {
-					clockStatusUI.text = `$(watch) ${countdown}s`;
-					countdown--;
-			}
-	}, 1000);
-}
-let clockStatusUI: vscode.StatusBarItem;
-let defaultDelta = 0.5;
-export function activate(context: vscode.ExtensionContext) {
-	let previousContent: string | undefined = undefined;
-	let typingCount = 0;
-  vscode.workspace.onDidOpenTextDocument((document: vscode.TextDocument) => {
-		previousContent = document.getText();
-	});	
-	vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
-    // 텍스트 문서가 변경될 때마다 실행 횟수 증가
-		if (event.contentChanges.length > 0) {
-			typingCount = typingCount + defaultDelta;
-		}
-	});
-  vscode.workspace.onDidSaveTextDocument((document: vscode.TextDocument) => {
-    if (typingCount > 0) {
-				clockStatusUI.show();
-        startCountdown(Math.ceil(typingCount));
-    }
-    typingCount = 0; // Save 후 실행 횟수 초기화
-	});
-	clockStatusUI = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
-	context.subscriptions.push(vscode.commands.registerCommand('extension.Tick-tock-clock: stop', () => {
-		vscode.window.showInformationMessage('Hello!');
-	}));
 
+const STATUS_BAR_ALIGNMENT = vscode.StatusBarAlignment.Left;
+const STATUS_BAR_PRIORITY = 100;
+const INTERVAL_MILLISECONDS = 1000;
+const DEFAULT_TYPING_INCREMENT = 0.5;
+
+let countdownTimer: NodeJS.Timeout | undefined;
+let clockStatusBarItem: vscode.StatusBarItem;
+let previousContent: string | undefined = undefined;
+let typingIncrement = 0;
+
+export function activate(context: vscode.ExtensionContext) {
+  initializeClockStatusBarItem();
+
+  vscode.workspace.onDidOpenTextDocument((document: vscode.TextDocument) => {
+    previousContent = document.getText();
+  });
+
+  vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
+    if (event.contentChanges.length > 0) {
+      typingIncrement += DEFAULT_TYPING_INCREMENT;
+    }
+  });
+
+  vscode.workspace.onDidSaveTextDocument(() => {
+    if (typingIncrement > 0) {
+      startCountdown(Math.ceil(typingIncrement));
+      resetTypingIncrement();
+    }
+  });
+
+  context.subscriptions.push(vscode.commands.registerCommand('extension.Tick-tock-clock: stop', () => {
+    vscode.window.showInformationMessage('Hello!');
+  }));
 }
-export function deactivate() {}
+
+export function deactivate() {
+  if (countdownTimer) {
+    clearInterval(countdownTimer);
+  }
+}
+
+function initializeClockStatusBarItem() {
+  clockStatusBarItem = vscode.window.createStatusBarItem(STATUS_BAR_ALIGNMENT, STATUS_BAR_PRIORITY);
+  clockStatusBarItem.text = "$(watch) Ready";
+  clockStatusBarItem.show();
+}
+
+function startCountdown(duration: number) {
+  let remainingTime = duration;
+	clockStatusBarItem.text = `$(watch) ${remainingTime}s`;
+  clearInterval(countdownTimer);
+  countdownTimer = setInterval(() => {
+    if (remainingTime <= 0) {
+      clearInterval(countdownTimer);
+      clockStatusBarItem.text = `$(watch) Ready`;
+    } else {
+			remainingTime--;
+      clockStatusBarItem.text = `$(watch) ${remainingTime}s`;
+    }
+  }, INTERVAL_MILLISECONDS);
+}
+
+function resetTypingIncrement() {
+  typingIncrement = 0;
+}
