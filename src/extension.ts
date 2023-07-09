a88d25a01719d33358eb8819692f4f8112fbc108
egoing <egoing@gmail.com>
2023-07-09 12:10:43 +0900

    타이핑 타임 보너스 값을 변경 할 수 있도록 개선

--- src/extension.ts
+++ src/extension.ts
@@ -3,7 +3,7 @@ import * as vscode from "vscode";
 const STATUS_BAR_ALIGNMENT = vscode.StatusBarAlignment.Left;
 const STATUS_BAR_PRIORITY = 100;
 const INTERVAL_MILLISECONDS = 1000;
-const DEFAULT_TYPING_INCREMENT = 0.5;
+let DEFAULT_TYPING_DELTA = 0.5;
 
 let countdownTimer: NodeJS.Timeout | undefined;
 let clockStatusBarItem: vscode.StatusBarItem;
@@ -17,12 +17,14 @@ export function activate(context: vscode.ExtensionContext) {
     previousContent = document.getText();
   });
 
-  vscode.workspace.onDidChangeTextDocument((event: vscode.TextDocumentChangeEvent) => {
-    if (event.contentChanges.length > 0) {
-      typingIncrement += DEFAULT_TYPING_INCREMENT;
-      clockStatusBarItem.text = `$(watch) ${prettyTime(typingIncrement)}`;
+  vscode.workspace.onDidChangeTextDocument(
+    (event: vscode.TextDocumentChangeEvent) => {
+      if (event.contentChanges.length > 0) {
+        typingIncrement += DEFAULT_TYPING_DELTA;
+        clockStatusBarItem.text = `$(watch) ${prettyTime(typingIncrement)}`;
+      }
     }
-  });
+  );
 
   vscode.workspace.onDidSaveTextDocument(() => {
     if (typingIncrement > 0) {
@@ -31,9 +33,22 @@ export function activate(context: vscode.ExtensionContext) {
     }
   });
 
-  context.subscriptions.push(vscode.commands.registerCommand('extension.Tick-tock-clock: stop', () => {
-    vscode.window.showInformationMessage('Hello!');
-  }));
+  vscode.commands.registerCommand(
+    "extension.changeTypingDelta",
+    async () => {
+      const newValueStr = await vscode.window.showInputBox({
+        prompt: "Typing time bonus",
+        value: DEFAULT_TYPING_DELTA.toString(),
+      });
+
+      if (newValueStr !== undefined) {
+        DEFAULT_TYPING_DELTA = parseFloat(newValueStr);
+        vscode.window.showInformationMessage(
+          `New typing time bonus is ${DEFAULT_TYPING_DELTA}`
+        );
+      }
+    }
+  );
 }
 
 export function deactivate() {
@@ -45,10 +60,10 @@ export function deactivate() {
 function prettyTime(seconds: number): string {
   const hrs = Math.floor(seconds / 3600);
   const mins = Math.floor((seconds % 3600) / 60);
-  let secs:any = seconds % 60;
-  secs = secs.toFixed(1);  // 소수점 첫째자리까지 포함하도록 변경
+  let secs: any = seconds % 60;
+  secs = secs.toFixed(1); // 소수점 첫째자리까지 포함하도록 변경
 
-  let result = '';
+  let result = "";
   if (hrs > 0) {
     result += `${hrs}h `;
   }
@@ -60,23 +75,26 @@ function prettyTime(seconds: number): string {
   return result;
 }
 
-
 function initializeClockStatusBarItem() {
-  clockStatusBarItem = vscode.window.createStatusBarItem(STATUS_BAR_ALIGNMENT, STATUS_BAR_PRIORITY);
+  clockStatusBarItem = vscode.window.createStatusBarItem(
+    STATUS_BAR_ALIGNMENT,
+    STATUS_BAR_PRIORITY
+  );
   clockStatusBarItem.text = "$(watch)";
   clockStatusBarItem.show();
+  clockStatusBarItem.command = "extension.changeTypingDelta";
 }
 
 function startCountdown(duration: number) {
   let remainingTime = duration;
-	clockStatusBarItem.text = `$(watch) ${prettyTime(remainingTime)}`;
+  clockStatusBarItem.text = `$(watch) ${prettyTime(remainingTime)}`;
   clearInterval(countdownTimer);
   countdownTimer = setInterval(() => {
     if (remainingTime <= 0) {
       clearInterval(countdownTimer);
       clockStatusBarItem.text = `$(watch)`;
     } else {
-			remainingTime--;
+      remainingTime--;
       clockStatusBarItem.text = `$(watch) ${prettyTime(remainingTime)}`;
     }
   }, INTERVAL_MILLISECONDS);
